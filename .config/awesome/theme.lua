2826ced5c6de16a5e6293906cad465eb01da3d75
croisen <absolutenico32@gmail.com>
2022-07-21 05:13:41 +0800

    Changed the wallpaper path

--- .config/awesome/theme.lua
+++ .config/awesome/theme.lua
@@ -98,7 +98,7 @@ theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/m
 theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
 
 -- Wallpapers
-theme.wallpaper = "~/.config/awesome/wallpapers/0000000.png"
+theme.wallpaper = "~/git/croi-wallpapers/0000000.png"
 
 
 -- You can use your own layout icons like this:
