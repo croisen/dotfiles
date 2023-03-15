ec08bd69b63eb52ca0571a9d0933c3002cfcc9dd
croisen <absolutenico32@gmail.com>
2023-03-15 16:47:45 +0800

    Changed how it loads the modules

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,15 @@
 -- My init.vim converted to init.lua
-require("lua.user.editor_opts")
-require("lua.user.plugins")
-require("lua.user.colorscheme")
-require("lua.user.keybinds")
-require("lua.user.treesitter")
+
+local config_files = {
+	"editor_opts.lua",
+	"plugins.lua",
+	"colorscheme.lua",
+	"keybinds.lua",
+	"treesitter.lua",
+}
+
+-- Dunno if I need to load it like this or require can just be used
+for _, file in ipairs(config_files) do
+	local path = string.format("%s/lua/user/%s", vim.fn.stdpath("config"), file)
+	vim.cmd("source " .. path)
+end
