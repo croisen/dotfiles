d2dc35ac625c85cd802bd8bfe584eddf4250a2a4
croisen <absolutenico32@gmail.com>
2023-04-10 12:00:21 +0800

    Moar coc-extensions

--- .config/nvim/lua/user/editor_opts.lua
+++ .config/nvim/lua/user/editor_opts.lua
@@ -2,6 +2,16 @@ local cache_dir = os.getenv("HOME") .. "/.cache/nvim"
 
 vim.g.rehash256 = 1
 -- vim.g.UltiSnipsSnippetDirectories = os.getenv("HOME") .. "/.config/nvim/plugged/vim-snippets/UltiSnips"
+vim.g.coc_global_extensions = { 'coc-clangd',
+    'coc-css',
+    'coc-git',
+    'coc-html-css-support',
+    'coc-jedi',
+    'coc-json',
+    'coc-kotlin',
+    'coc-lua',
+    'coc-prettier',
+    'coc-python' }
 vim.opt.autoindent = true
 vim.opt.backupdir = cache_dir
 vim.opt.cc = "100"
@@ -14,8 +24,8 @@ vim.opt.nu = true
 vim.opt.rnu = true
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
-vim.opt.mouse= a
-vim.opt.mouse= v
+vim.opt.mouse=a
+vim.opt.mouse=v
 vim.opt.shiftwidth = 4
 vim.opt.showmatch = true
 vim.opt.showmode = false
@@ -26,5 +36,3 @@ vim.opt.tabstop=4
 vim.opt.termguicolors = true
 vim.opt.ttyfast = true
 vim.opt.wildmode = {'longest', 'list'}
-vim.cmd("set nocompatible")
-vim.cmd("tab all")
