0a540a87dc5c88e9191912154555ba03137a471e
croisen <andrewjames.git.gan@gmail.com>
2024-10-24 10:03:57 +0800

    Lsp+Quickfix

--- nvim/lua/config/keybinds.lua
+++ nvim/lua/config/keybinds.lua
@@ -1,34 +1,39 @@
 local key_set = vim.keymap.set
+local opts = { noremap = true, silent = true, }
+
+local function quickfix()
+    vim.lsp.buf.code_action({})
+end
 
 -- Move the lines around, whether in normal or visual mode
-key_set('i', '<c-up>', '<esc>:m .-2<cr>==gi')
-key_set('i', '<c-down>', '<esc>:m .+1<cr>==gi')
-key_set('v', '<c-up>', ":m '<-2<cr>gv=gv")
-key_set('v', '<c-down>', ":m '>+1<cr>gv=gv")
+key_set('i', '<c-up>', '<esc>:m .-2<cr>==gi', opts)
+key_set('i', '<c-down>', '<esc>:m .+1<cr>==gi', opts)
+key_set('v', '<c-up>', ":m '<-2<cr>gv=gv", opts)
+key_set('v', '<c-down>', ":m '>+1<cr>gv=gv", opts)
 
 -- Move the placement of the split
-key_set('n', '<a-up>', "<c-w>K")
-key_set('n', '<a-left>', "<c-w>H")
-key_set('n', '<a-down>', "<c-w>J")
-key_set('n', '<a-right>', "<c-w>L")
+key_set('n', '<a-up>', "<c-w>K", opts)
+key_set('n', '<a-left>', "<c-w>H", opts)
+key_set('n', '<a-down>', "<c-w>J", opts)
+key_set('n', '<a-right>', "<c-w>L", opts)
 
 -- Move to other splits
-key_set('n', '<c-up>', ":wincmd k<cr>")
-key_set('n', '<c-left>', ":wincmd h<cr>")
-key_set('n', '<c-down>', ":wincmd j<cr>")
-key_set('n', '<c-right>', ":wincmd l<cr>")
+key_set('n', '<c-up>', ":wincmd k<cr>", opts)
+key_set('n', '<c-left>', ":wincmd h<cr>", opts)
+key_set('n', '<c-down>', ":wincmd j<cr>", opts)
+key_set('n', '<c-right>', ":wincmd l<cr>", opts)
 
 -- Resizing the splits
-key_set('n', '<c-a-up>', ":2wincmd +<cr>")
-key_set('n', '<c-a-left>', ":2wincmd <<cr>")
-key_set('n', '<c-a-down>', ":2wincmd -<cr>")
-key_set('n', '<c-a-right>', ":2wincmd ><cr>")
+key_set('n', '<c-a-up>', ":2wincmd +<cr>", opts)
+key_set('n', '<c-a-left>', ":2wincmd <<cr>", opts)
+key_set('n', '<c-a-down>', ":2wincmd -<cr>", opts)
+key_set('n', '<c-a-right>', ":2wincmd ><cr>", opts)
 
 -- Escape terminal mode
-key_set('t', '<esc>', "<c-\\><c-n>")
+key_set('t', '<esc>', "<c-\\><c-n>", opts)
 
 -- Remove highlight search
-key_set('n', '<Leader>n', ":noh<cr>")
+key_set('n', '<Leader>n', ":noh<cr>", opts)
 
 -- Open nvim-tree
 key_set('n', '<Leader>f', ":NvimTreeToggle<cr>")
@@ -36,16 +41,19 @@ key_set('n', '<Leader>f', ":NvimTreeToggle<cr>")
 -- Telescope
 local ok, tele_key = pcall(require, 'telescope.builtin')
 if ok then
-    key_set('n', '<leader>ff', tele_key.find_files, {})
-    key_set('n', '<leader>fg', tele_key.live_grep, {})
-    key_set('n', '<leader>fb', tele_key.buffers, {})
-    key_set('n', '<leader>fh', tele_key.help_tags, {})
+    key_set('n', '<leader>ff', tele_key.find_files, opts)
+    key_set('n', '<leader>fg', tele_key.live_grep, opts)
+    key_set('n', '<leader>fb', tele_key.buffers, opts)
+    key_set('n', '<leader>fh', tele_key.help_tags, opts)
 end
 
 local ok, hex_view = pcall(require, 'hex')
 if ok then
-    key_set('n', '<leader>hh', hex_view.toggle, {})
+    key_set('n', '<leader>hh', hex_view.toggle, opts)
 end
+
+-- QuickFix
+key_set('n', '<leader>qf', quickfix, opts)
 
 -- Bufferline
 --key_set('n', 'gb', ":BufferLinePick<cr>", {})
