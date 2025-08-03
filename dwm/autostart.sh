b1eec1f5102957a835ee5ee0ef8ac4a18f282bfa
croisen <andrewjames.git.gan@gmail.com>
2025-08-03 17:02:53 +0800

    dwm and hyprland tweaks again

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -3,11 +3,9 @@
 exec_once()
 {
     if command -v $1 >/dev/null; then
-        pgrep -u $USER $1 || $@ &
+        pgrep -f -u $USER $1 || $@ &
     fi
 }
 
 exec_once /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
 exec_once slstatus
-exec_once picom
-exec_once vesktop
